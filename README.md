624021232dc6075a8b091194ff8f45033d6ea123
Huynh Bao Minh <hbminh97bt@gmail.com>
2020-09-24 18:24:46 +0700

    create catelogy in entities

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # ASP.NET core 3.1 project from Minh
 
 ## Technologist
+## Install Packaged
+Microsoft.EntityFrameworkCore.SqlServer
+Microsoft.EntityFrameworkCore.Design
+Microsoft.EntityFrameworkCore.Tools
---
+++ Solution.Data/EF/SolutionDbContent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Solution.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution.Data.EF
+{
+    public class SolutionDbContent : DbContext
+    {
+        public SolutionDbContent(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Catelogy> Catelogies { get; set; }
+    }
+
+}
+   
